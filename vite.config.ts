659bd65285849bb775a28ba3b41296de54ded968
Valentina-K <kvalentyna@hotmail.com>
2026-04-24 10:13:11 +0300

    fix packege.json, vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/octen-react",
+    base: "/octen-react-test",
     plugins: [react(), tailwindcss(),],
 })
